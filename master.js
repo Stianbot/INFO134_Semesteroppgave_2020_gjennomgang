1c34fc1a5c745aa180ff97fc13181f71df2fa5f1
Stian <stian.botnevik@student.uib.no>
2020-03-17 10:50:27 +0100

    Oppdatert etter lab (gruppe 1)

--- master.js
+++ master.js
@@ -5,7 +5,11 @@ Det er ikke garantert at dette er den beste / mest effektive måten å løse opp
 Dette er ikke en fasit på semesteroppgaven. Det skal bare brukes til veiledning og gi inspirasjon til hvordan oppgavene kan løses.
 */
 
+function urler() {
+  return ["https://opencom.no/dataset/47f1e434-1a51-4deb-a66d-1387dfcc636e/resource/d5254e27-7fe7-4efa-aeeb-4582337c277f/download/52hverdagsturertekst.json","http://wildboy.uib.no/~tpe056/folk/85432.json", "http://wildboy.uib.no/~tpe056/folk/100145.json" ]}
+
 var turer = "https://opencom.no/dataset/47f1e434-1a51-4deb-a66d-1387dfcc636e/resource/d5254e27-7fe7-4efa-aeeb-4582337c277f/download/52hverdagsturertekst.json";
+var utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
 function load(url, func){
     var xhr = new XMLHttpRequest();
@@ -13,25 +17,118 @@ function load(url, func){
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4 && xhr.status === 200) {
           // NOTE: lager en global variabel: data, som inneholder datasettet. parser responsen
-            data = JSON.parse(xhr.responseText);
-            console.log(data);
+            //data = JSON.parse(xhr.responseText);
+            //console.log(data);
             // NOTE: hvis det finnes en funksjon som blir oppgitt i parameteret; kall på denne funksjonen.
-            if (func) {
-              func(data)
-            }
     };
 }
     xhr.send()
 }
 
+
+function last(url, callback) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url)
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      var data = JSON.parse(request.responseText)
+      console.log(data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1] );
+      if (callback) {
+        callback(data)
+      }
+
+    };
+  }
+  request.send()
+}
+
+//last(utdanning, function(x) {last(turer, function() {console.log("Ferdig lastet inn");})})
+//last(turer, utdanning_last)
+
+last(turer, function (y) {program(y)})
+
+function print_coordinates(arg) {
+  var plassering = document.getElementById("ul_oversikt")
+  console.log(plassering);
+  for (var variable in arg.features) {
+    var li = document.createElement("H1")
+    var text = document.createTextNode(arg.features[variable].geometry.coordinates[0] + " " + arg.features[variable].geometry.coordinates[1])
+    li.appendChild(text)
+    plassering.appendChild(li)
+  }
+jkj
+}
+
+function program(argument) {
+  print_coordinates(argument)
+  print_asdasd(argument)
+  calculate_population(argument)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function utdanning_last() {
+  last(ut)
+}
+
+
+
+
+
+function print_data(argument) {
+  console.log("Her printes data",argument);
+}
+
+//print_data(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+function pluss(a, b) {
+  return a + b
+}
+
+var tall = pluss
+var tall2 = pluss(40, 60)
+var tall3 = function() {return pluss(70,40)}
+
+
+
+
+
+
 // NOTE: funksjon som henter ned turer og printer ut "ferdig lastet inn" når data er ferdig lastet inn.
 // NOTE: Bruker en callback funksjon til å få dette til.
 // NOTE: callback funksjonen lager også ett objekt av typen interface og logger dette til konsollen.
-load(turer,function(){
-  console.log("Ferdig lastet inn");
+/*load(turer,function(){
+  //console.log("Ferdig lastet inn");
   // NOTE: lagrer grensesnitt som global variabel
   grensesnitt = new Interface(data)
-})
+})*/
 
 // NOTE: konstruktør for et brukergrensesnitt, tar inn ett argument (datasett)
 // NOTE: metoden getCoordinates kaller på funksjonen "coordinates"
@@ -80,3 +177,20 @@ function hent_input() {
     return input
   }
 }
+
+// NOTE: fiks eksempel
+function give_class(id) {
+  var intro = document.getElementById(id)
+
+  if (intro.className == "farge") {
+    //console.log("elementet er farget");
+    intro.classList.add("ikke_farge")
+  }
+
+  if (intro.className != "farge") {
+    intro.className = "farge"
+  }
+}
+
+//give_class("Introduksjon")
+//give_class("Detaljer")
