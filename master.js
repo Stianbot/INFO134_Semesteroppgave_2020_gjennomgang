b0f52066a85590657f12bdfb74f9f62ba10c78f7
Stian <stian.botnevik@student.uib.no>
2020-03-17 11:13:53 +0100

    Oppdatert etter lab (gruppe 1)

--- master.js
+++ master.js
@@ -5,36 +5,18 @@ Det er ikke garantert at dette er den beste / mest effektive måten å løse opp
 Dette er ikke en fasit på semesteroppgaven. Det skal bare brukes til veiledning og gi inspirasjon til hvordan oppgavene kan løses.
 */
 
-function urler() {
-  return ["https://opencom.no/dataset/47f1e434-1a51-4deb-a66d-1387dfcc636e/resource/d5254e27-7fe7-4efa-aeeb-4582337c277f/download/52hverdagsturertekst.json","http://wildboy.uib.no/~tpe056/folk/85432.json", "http://wildboy.uib.no/~tpe056/folk/100145.json" ]}
-
 var turer = "https://opencom.no/dataset/47f1e434-1a51-4deb-a66d-1387dfcc636e/resource/d5254e27-7fe7-4efa-aeeb-4582337c277f/download/52hverdagsturertekst.json";
 var utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
-function load(url, func){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          // NOTE: lager en global variabel: data, som inneholder datasettet. parser responsen
-            //data = JSON.parse(xhr.responseText);
-            //console.log(data);
-            // NOTE: hvis det finnes en funksjon som blir oppgitt i parameteret; kall på denne funksjonen.
-    };
-}
-    xhr.send()
-}
-
-
-function last(url, callback) {
+function last(url, func) {
   var request = new XMLHttpRequest();
   request.open("GET", url)
   request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
       var data = JSON.parse(request.responseText)
-      console.log(data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1] );
-      if (callback) {
-        callback(data)
+      //console.log(data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1] );
+      if (func) {
+        func(data)
       }
 
     };
@@ -48,46 +30,32 @@ function last(url, callback) {
 last(turer, function (y) {program(y)})
 
 function print_coordinates(arg) {
+  // NOTE: henter plasseringen hvor jeg vil legge til html-elementer (henter ut ett html-element)
   var plassering = document.getElementById("ul_oversikt")
-  console.log(plassering);
   for (var variable in arg.features) {
-    var li = document.createElement("H1")
+    // NOTE: lager li element
+    var li = document.createElement("li")
+    // NOTE: lager tekst som inneholder koordinater til turer.
     var text = document.createTextNode(arg.features[variable].geometry.coordinates[0] + " " + arg.features[variable].geometry.coordinates[1])
+    // NOTE: fester tekst til li-element
     li.appendChild(text)
+    // NOTE: fester li-element til html-elementet jeg har hentet ut som blir plasseringen på html-siden.
+    // NOTE: merk at jeg har hentet ut et UL-element og lager li-elementer i js som jeg fester til dette ul elemntet.
     plassering.appendChild(li)
   }
-jkj
 }
 
+
+// NOTE: funksjon som skal illustrere at en kan ha en funksjon som kaller på mange andre funksjoner
+// NOTE: denne funksjonen kan kalles etter at alle ressursene er lastet ned, og fungere som "oppsett" til nettsiden.
 function program(argument) {
   print_coordinates(argument)
+
+/* //Disse finnes ikke, de er bare ment som eksempler
   print_asdasd(argument)
   calculate_population(argument)
+  */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function utdanning_last() {
-  last(ut)
-}
-
-
-
 
 
 function print_data(argument) {
@@ -97,38 +65,25 @@ function print_data(argument) {
 //print_data(data)
 
 
-
-
-
-
-
-
-
-
-
-
-
 function pluss(a, b) {
   return a + b
 }
 
+
+// NOTE: prøver å vise bruksområdet til en anonym funksjon
 var tall = pluss
 var tall2 = pluss(40, 60)
 var tall3 = function() {return pluss(70,40)}
 
 
-
-
-
-
 // NOTE: funksjon som henter ned turer og printer ut "ferdig lastet inn" når data er ferdig lastet inn.
 // NOTE: Bruker en callback funksjon til å få dette til.
 // NOTE: callback funksjonen lager også ett objekt av typen interface og logger dette til konsollen.
-/*load(turer,function(){
+last(turer,function(y){
   //console.log("Ferdig lastet inn");
   // NOTE: lagrer grensesnitt som global variabel
-  grensesnitt = new Interface(data)
-})*/
+  grensesnitt = new Interface(y)
+})
 
 // NOTE: konstruktør for et brukergrensesnitt, tar inn ett argument (datasett)
 // NOTE: metoden getCoordinates kaller på funksjonen "coordinates"
@@ -178,7 +133,7 @@ function hent_input() {
   }
 }
 
-// NOTE: fiks eksempel
+// NOTE: fiks eksempel.
 function give_class(id) {
   var intro = document.getElementById(id)
 
