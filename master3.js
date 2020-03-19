74bed0690f58d957db7102fa4618220960b005d8
Stian <stian.botnevik@student.uib.no>
2020-03-19 11:57:09 +0100

    Kommentert kode etter lab 19.03.20

--- master3.js
+++ master3.js
@@ -1,53 +1,39 @@
-
+// NOTE: Eksempel på en konstruktør
 function Tur(url, distanse, tid){
   this.url = url
   this.distanse = distanse
   this.tid = tid
+  // NOTE: eksempel på metode i en konstruktør
   this.fart = function() {return fartutregning(this.distanse, this.tid)}
 }
 
+// NOTE: eksempel på en konstruktør
 function Grensesnitt(url) {
   this.url = url
-  this.getNames = function(){return fartutregning(40,20){}}
+  // NOTE: benytter en anonym funksjon
+  this.fart = function(){return fartutregning(40,20){}}
 }
 
-function Grensesnitt(url) {
-  this.url = url
-  this.getNames = function(){return fartutregning(40,20){}}
-}
-
-
+// NOTE: enkel funksjon, deler d(distanse) på t(tid) og returnerer resultatet
 function fartutregning2(d,t){
   return d/t
 }
 
-
+// NOTE: oppretter objekter av typen "Grensesnitt" ved hjelp av konstruktøren definert over
 var gggg = new Grensesnitt("url.no")
 var ffff = new Grensesnitt("sjsj")
-
 var befolkning = new Grensesnitt("url til befolkning")
 var utdanning = new Grensesnitt("url til utdanning")
 var sysselsetting = new Grensesnitt("url til sysselsetting")
 
 
-befolkning.load()
-utdanning.load()
-sysselsetting.load()
-
-function getNames() {
-
-}
-
-befolkning.getNames()
-
+// NOTE: oppretter et objekt av typen "Tur" ved hjelp av konstruktør
 var min_tur = new Tur("www.ut.no", 50, 5)
 console.log(min_tur);
 
 
-function fartutregning(d,t){
-  return d/t
-}
 
+// NOTE: viser at det er bedre å lage en generell funksjon (minus) istedet for å ha mange lignende funksjoner som gjør det samme, bare med små variasjoner.
 function minus1() {
   return 100 - 50
 }
@@ -63,7 +49,9 @@ function minus3() {
 minus1()
 minus2()
 minus3()
+// NOTE: -----------------------------------------------------------------------
 
+// NOTE: koden under gjør det samme som koden over, bare på mye mindre plass, og fungerer for alle tall.
 function minus(x,y) {
   return x - y
 }
@@ -71,10 +59,16 @@ function minus(x,y) {
 minus(100,50)
 minus(70,30)
 minus(50,10)
+// NOTE: -----------------------------------------------------------------------
 
 
+// NOTE: Demonstrerer bruksområdet ved å bruke en anonym funksjon.
+// NOTE: a vil bli resultatet av minus(100,70) funksjonen
 var a = minus(100, 70)
+// NOTE: b vil bli selve minus funksjonen
 var b = minus
+// NOTE: samme som b, men en har muligheter til å bruke argumenter i et funksjonskall uten at dette blir kjørt
+// NOTE: en kan også kalle på så mange funksjoner en ønsker her. Ulikt b, som bare kan være en funksjon.
 var c = function() {
   var resultat = minus(100, 70)
   console.log("Logger resultat", resultat);
